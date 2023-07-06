d010a1dd0d96bfa421c69091bd842249d6c6b502
namanlp <naman@garudalinux.org>
2023-07-07 02:44:47 +0530

    Done Pascal Triangle

---
+++ day_1_arrays/src/pascal_triangle.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let n = num_rows as usize;
+        let mut ans = Vec::with_capacity(n+3);
+        ans.push(vec![1]);
+        for i in 1..n {
+            let mut v = Vec::with_capacity(i + 3);
+            v.push(1);
+
+            if i >= 2 {
+                for j in 0..i-1 {
+                    v.push(ans[i-1][j] + ans[i-1][j+1]);
+                }
+            }
+            v.push(1);
+            ans.push(v);
+        }
+        return ans;
+    }
+}
