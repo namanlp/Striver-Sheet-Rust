312eac3ac3694e4d68397beac1bb5eacd7a37d33
namanlp <naman@garudalinux.org>
2023-07-08 00:22:03 +0530

    Done merge intervals

---
+++ day_2_arrays/src/merge_intervals.rs
@@ -0,0 +1,24 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut intervals = intervals;
+        intervals.sort();
+
+        let mut ans = Vec::with_capacity(intervals.len());
+
+        let mut current =  intervals[0].clone();
+
+        for i in 1..intervals.len() {
+            if intervals[i][0] <= current[1] {
+                current[1] = max(intervals[i][1], current[1]);
+                continue;
+            }
+            ans.push(current.clone());
+            current = intervals[i].clone();
+        }
+        ans.push(current.clone());
+
+        return ans;
+    }
+}
