4850f7efa840109f8f78107bbe126c9899814807
namanlp <naman@garudalinux.org>
2023-07-10 15:50:02 +0530

    Explain find duplicate

--- day_2_arrays/src/find_duplicate.rs
+++ day_2_arrays/src/find_duplicate.rs
@@ -2,7 +2,13 @@
 // My solution Time complexity : O(n)
 // My solution Space complexity : O(1)
 
+
 impl Solution {
+    // My solution uses hash table for the solution.
+    // But instead of using new vector, we store the numbers at index in original array itself.
+
+    // We know that all the elements are from 1 to n
+    // So, we can use n+1 as counter to store the frequency of the elements in the table itself.
     pub fn find_duplicate(mut nums: Vec<i32>) -> i32 {
         let num = nums.len()+3;
 
