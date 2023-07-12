5e2b5d1f3f0270231674a18bf916d716724aedba
namanlp <naman@garudalinux.org>
2023-07-12 23:50:01 +0530

    Done majority element n/3

---
+++ day_3_arrays/src/majority_element_n_3.rs
@@ -0,0 +1,26 @@
+use std::collections::BTreeMap;
+
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> Vec<i32> {
+        let mut frequency : BTreeMap<i32, usize> = BTreeMap::new();
+        let n = nums.len();
+
+        for number in nums {
+            let val = frequency.get(&number);
+            if val.is_none() {
+                frequency.insert(number, 1);
+            }else {
+                let val = *val.unwrap();
+                frequency.insert(number, val+1);
+            }
+        }
+        let mut ans = Vec::new();
+        for (k, v) in frequency {
+            if v>=n/3 {
+                ans.push(k);
+            }
+        }
+
+        return ans;
+    }
+}
